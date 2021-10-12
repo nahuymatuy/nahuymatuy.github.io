14e5de7a0e644dd92831937ab91cd10caee6026a
s9921316 <audiowalk.art@gmail.com>
2021-10-12 21:49:17 +0800

    ok
    Description ..

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,17 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
       ? '/'
-      : '/'
-      ? '/tutorials'
-      : '/tutorials'
-      ? '/add'
-      : '/add'
-      ? '/12'
-      : '/add'
+      : '/' ,
+      outputDir: "dist",
+      lintOnSave: true,
+      runtimeCompiler: false,
+      productionSourceMap: false,
+      devServer: {
+        open: process.platform === "darwin",
+        port: 8000,
+        https: false,
+        hotOnly: false,
+        proxy: null,
+        disableHostCheck: true,
+      }
   }
