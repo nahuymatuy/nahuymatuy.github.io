80b415aced04e8f81236b78d3f4c27df3a7f36e8
s9921316 <audiowalk.art@gmail.com>
2021-10-12 21:27:43 +0800

    ok
    Description ..

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,10 @@ module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
       ? '/'
       : '/'
+      ? '/tutorials'
+      : '/tutorials'
+      ? '/add'
+      : '/add'
+      ? '/12'
+      : '/add'
   }
